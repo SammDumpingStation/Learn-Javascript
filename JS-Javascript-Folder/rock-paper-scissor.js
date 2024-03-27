378570c35a6131d49e4b74935a6fedd9e55db526
Samm's Dumping Station <sammcaag@gmail.com>
2024-03-27 08:28:30 +0800

    😮‍💨 Fixed For Loop for History Log

--- JS-Javascript-Folder/rock-paper-scissor.js
+++ JS-Javascript-Folder/rock-paper-scissor.js
@@ -1,12 +1,14 @@
 let yourMove = "";
 let points = JSON.parse(localStorage.getItem("scoreTotal"));
-const userArray = [];
-const compArray = [];
+const winLoseDisplay = [];
+const historyLogArray = [];
 const resultArray = [];
 
 // For dark mode later -->
 // const html = document.querySelector("html");
 // html.style.backgroundColor = "black";
+
+// functions section
 
 function scoreStorage() {
   localStorage.setItem("scoreTotal", JSON.stringify(points));
@@ -25,6 +27,21 @@ function fightResult(userMove) {
     computerPick = "Scissors";
   }
 
+  result = winOrLose(computerPick, userMove);
+
+  if (result === "You Win") {
+    points.User++;
+  } else if (result === "You lose") {
+    points.Computer++;
+  } else if (result === "Tie") {
+    points.Tie++;
+  }
+  renderScore(userMove, computerPick, result);
+  renderLog(userMove);
+}
+
+function winOrLose(computerPick, userMove) {
+  let result = '';
   if (computerPick === "Rock") {
     if (userMove === "Rock") {
       result = "Tie";
@@ -47,85 +64,82 @@ function fightResult(userMove) {
     } else if (userMove === "Paper") {
       result = "You lose";
     } else if (userMove === "Scissors") {
-      result = "It's a tie";
+      result = "Tie";
     }
   }
-
-  if (result === "You Win") {
-    points.User++;
-  } else if (result === "You lose") {
-    points.Computer++;
-  } else if (result === "Tie") {
-    points.Tie++;
-  }
-  renderScore(userMove, computerPick, result);
-  addingArray("Start");
-  renderLog();
+  return result;
 }
 
 function renderScore(user, comp, output) {
   let render = document.querySelector(".render-score");
   let message = "";
+  let checkedUserMove = '';
+  let checkedComputerMove = '';
+  let checkedResult  = '';
+  let divInsideHistoryLog = '';
   if (user === "reset" || comp === "reset" || output === "reset") {
     render.innerHTML = `Scores have been reseted to 0! Start picking a move again!`;
     message = `<p class="rendered-log">Scores have been reseted to 0!</p>`;
-    userArray.unshift(message);
+    winLoseDisplay.unshift(message);
   } else {
     render.innerHTML = `You picked ${user} while Computer picked ${comp}. ${output}. User: ${points.User} Computer: ${points.Computer} Tie: ${points.Tie}.`;
     message = `User: ${user}. Computer: ${comp}. Result: ${output}`;
-    userArray.unshift(message);
-  }
+    winLoseDisplay.unshift(message);
 
-  if (user === 'Rock') {
-    document.querySelector(".user").innerHTML =`<img src="Images-Folder/rockFrame.png" alt="rock" class="log-img user-img" /> <p class="user-description">${user}</p>`;
-    }
-    else if (user === 'Paper') {
-    document.querySelector(".user").innerHTML =`<img src="Images-Folder/paperFrame.png" alt="rock" class="log-img user-img" /> <p class="user-description">${user}</p>`;
-    }
-    else if (user === 'Scissor') {
-    document.querySelector(".user").innerHTML =`<img src="Images-Folder/scissorFrame.png" alt="rock" class="log-img user-img" /> <p class="user-description">${user}</p>`;
-    }
-    checksComputerMove(comp);
-    checksResult(output);
+    checkedUserMove = checkUserMove(user);
+    checkedResult = checksResult(output);
+    checkedComputerMove = checksComputerMove(comp);
+
+    divInsideHistoryLog = `<div class="log-div"> <div class="log-img-container user">${checkedUserMove}</div><div class="log-img-container result">${checkedResult}</div><div class="log-img-container computer">${checkedComputerMove}</div></div>`;
+    historyLogArray.unshift(divInsideHistoryLog);
   }
+}
+
+function checkUserMove(user) {
+  let checkedUserMove = "";
+    if (user === "Rock") {
+      checkedUserMove = `<img src="Images-Folder/rockFrame.png" alt="rock" class="log-img user-img" /> <p class="user-description">${user}</p>`;
+    } else if (user === "Paper") {
+      checkedUserMove = `<img src="Images-Folder/paperFrame.png" alt="rock" class="log-img user-img" /> <p class="user-description">${user}</p>`;
+    } else if (user === "Scissors") {
+      checkedUserMove = `<img src="Images-Folder/scissorFrame.png" alt="rock" class="log-img user-img" /> <p class="user-description">${user}</p>`;
+    }
+  return checkedUserMove;
+}
 
 function checksComputerMove(comp) {
+  let checkedComputerMove = '';
+
   if (comp === "Rock") {
-    document.querySelector(
-      ".computer"
-    ).innerHTML = `<img src="Images-Folder/rockFrame.png" alt="rock" class="log-img computer-img" /> <p class="computer-description">${comp}</p>`;
+    checkedComputerMove =  `<img src="Images-Folder/rockFrame.png" alt="rock" class="log-img computer-img" /> <p class="computer-description">${comp}</p>`;
   } else if (comp === "Paper") {
-    document.querySelector(
-      ".computer"
-    ).innerHTML = `<img src="Images-Folder/paperFrame.png" alt="paper" class="log-img computer-img" /> <p class="computer-description">${comp}</p>`;
+    checkedComputerMove = `<img src="Images-Folder/paperFrame.png" alt="paper" class="log-img computer-img" /> <p class="computer-description">${comp}</p>`;
   } else if (comp === "Scissors") {
-    document.querySelector(
-      ".computer"
-    ).innerHTML = `<img src="Images-Folder/scissorFrame.png" alt="scissor" class="log-img computer-img" /> <p class="computer-description">${comp}</p>`;
+    checkedComputerMove= `<img src="Images-Folder/scissorFrame.png" alt="scissor" class="log-img computer-img" /> <p class="computer-description">${comp}</p>`;
   }
+  return checkedComputerMove;
 }
 
 function checksResult(output) {
-  if (output === 'You Win') {
-    document.querySelector(".result").innerHTML = `<img src="Images-Folder/win.png" alt="" class="log-img win-img" /> <p class="result-description">${output}</p>`;    
+  let checkedResult = '';
+  if (output === "You Win") {
+    checkedResult = `<img src="Images-Folder/win.png" alt="" class="log-img win-img" /> <p class="result-description">${output}</p>`;
+  } else if (output === "You lose") {
+    checkedResult = `<img src="Images-Folder/lose.png" alt="" class="log-img lose-img" /> <p class="result-description">${output}</p>`;
+  } else if (output === "Tie") {
+    checkedResult = `<img src="Images-Folder/tie.png" alt="" class="log-img tie-img" /> <p class="result-description">${output}</p>`;
   }
-  else if (output === 'You lose') {
-    document.querySelector(
-      ".result"
-    ).innerHTML = `<img src="Images-Folder/lose.png" alt="" class="log-img lose-img" /> <p class="result-description">${output}</p>`;    
-  }
-  else if (output === 'Tie') {
-    document.querySelector(".result").innerHTML = `<img src="Images-Folder/tie.png" alt="" class="log-img tie-img" /> <p class="result-description">${output}</p>`;    
-  }
+  return checkedResult;
 }
 
-function renderLog() {
+function renderLog(user) {
   let logHTML = "";
-  for (let i = 0; i < userArray.length; i++) {
-    const logString = userArray[i];
-    const logElement = `<p class="rendered-log">${logString}</p>`;
+
+  for (let i = 0; i < historyLogArray.length; i++) {
+    const logString = historyLogArray[i];
+    const logElement = logString;
     logHTML += logElement;
   }
-  // document.querySelector(".history-logs-p").innerHTML = logHTML;
-  console.log((document.querySelector(".log-div").innerHTML = logHTML));
+  document.querySelector(".log-container").innerHTML = logHTML;
 }
+
