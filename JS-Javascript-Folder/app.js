4069269df5b262f9c2c63332a22f79f2ad169cb9
Samm Caagbay <sammcaag@gmail.com>
2024-03-30 22:31:59 +0800

    🙏 Practicing Nodejs (Wish me luck)

---
+++ JS-Javascript-Folder/app.js
@@ -0,0 +1,7 @@
+const arrayFromLogs = require('./log');
+
+const os = require('os');
+// console.log(__dirname);
+// console.log(__filename);
+
+console.log(os.platform(), os.homedir());
---
+++ JS-Javascript-Folder/log.js
@@ -0,0 +1,9 @@
+const dogs = ['Dalmatian', 'French Bakery', 'LaChihuahua', 'Water Poodle'];
+const tumblers = ['AquaFlask', 'HydroFlask', 'ChinaFlask', 'SacredFlask'];
+const price = [2000, 4000, 1000, 3000];
+
+// console.log(dogs);
+
+module.exports = {
+  dogs, tumblers, price
+}
